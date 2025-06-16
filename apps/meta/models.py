fc7c25ecaab677d25335a75f76357482104af42d
Sevinch <sevinchoblayorova81@gmail.com>
2025-06-16 20:34:13 +0500

    meta was added

--- apps/meta/models.py
+++ apps/meta/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.common.models import BaseModel
+from apps.movies.models import Movie
 
-# Create your models here.
+
+class MovieView(BaseModel):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    ip = models.CharField(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.movie} - {self.created_at}"
